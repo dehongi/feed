d067e0913f35ae030c4db45cd5a51c3c896ac571
Hamed Mohammadi <hamed@dehongi.com>
2024-11-20 10:12:08 +0330

    signup

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from .views import SignupView
-from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path("signup/", SignupView.as_view(), name="signup"),
